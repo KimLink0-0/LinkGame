214f76b834e6edd5d6e77eb0400fc8768ffd2b6a
KimLink <Linkedcirrus93@gmail.com>
2026-01-20 06:33:27 +0900

    [test]

---
+++ Source/LinkGame/GameMode/LinkExperienceDefinition.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LinkExperienceDefinition.h"
+
+ULinkExperienceDefinition::ULinkExperienceDefinition(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
---
+++ Source/LinkGame/GameMode/LinkGameState.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "LinkGameState.generated.h"
+
+
+class ULinkExperienceManagerComponent;
+
+UCLASS()
+class LINKGAME_API ALinkGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+public:
+	ALinkGameState();
+	
+public:
+	UPROPERTY()
+	TObjectPtr<ULinkExperienceManagerComponent> ExperienceManagerComponent;
+};
